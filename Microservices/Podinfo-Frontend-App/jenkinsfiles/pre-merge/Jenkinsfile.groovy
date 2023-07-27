361912f5d9bf487310bcbc32fe7a2d7324860ef5
Vijay Kancherla <vkancherla@Vijays-MacBook-Pro.local>
2023-07-27 21:25:19 +0530

    Feature-X

--- Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
+++ Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
@@ -14,6 +14,13 @@ stage("Package-Image") {
   dir('Microservices/podinfo-application-frontend')
   {
     script {
+
+        echo "Environment variables:"
+        echo "PROJECT: ${env.PROJECT}"
+        echo "REGISTRY_USER: ${env.REGISTRY_USER}"
+        echo "GIT_COMMIT_HASH: ${env.GIT_COMMIT_HASH}"
+        echo "IMAGE_REPO: ${env.IMAGE_REPO}"
+        echo "IMAGE_TAG: ${env.IMAGE_TAG}"
         docker.build('${IMAGE_REPO}:${IMAGE_TAG}', 'Dockerfile')
     }
   }
