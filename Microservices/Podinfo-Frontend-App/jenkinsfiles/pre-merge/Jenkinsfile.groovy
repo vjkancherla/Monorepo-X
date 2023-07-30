47a659307908c15079828a7e775ad0102a065a17
Vijay Kancherla <vkancherla@Vijays-MacBook-Pro.local>
2023-07-30 22:29:55 +0530

    Feature-X Sun 30 Jul 2023 22:29:55 IST

--- Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
+++ Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
@@ -44,12 +44,24 @@ stage('Deploy App to K3D Dev') {
 }
 
 stage('Test App in K3D Dev') {
-    sleep(30)
     script {
       withCredentials([file(credentialsId: 'k3d-config', variable: 'KUBECONFIG')]) {
         sh """
           export KUBECONFIG=${KUBECONFIG}
-          kubectl run curl --image=curlimages/curl -i --rm --restart=Never -- curl helm-pi-fe-dev.dev.svc.cluster.local:9898
+          for i in {1..30}; do
+            if kubectl get pods -n dev | grep Running; then
+              echo "Application is running!"
+              break
+            elif [ "$i" -eq "30" ]; then
+              echo "Application failed to start within the expected time."
+              exit 1
+            else
+              echo "Waiting for application to start..."
+              sleep 1
+            fi
+          done
+          service_name=$(kubectl get service -n dev -o jsonpath='{.items[*].metadata.name}')
+          kubectl run -n dev curl --image=curlimages/curl -i --rm --restart=Never -- curl ${service_name}:9898
         """
       }
     }
