7af2e93b32298fc838a3d073953a64900e8f5362
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-08-03 13:59:54 +0530

    Feature-X Thu  3 Aug 2023 13:59:54 IST

--- Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
+++ Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
@@ -1,29 +1,27 @@
 
 stage('Test App in K3D Dev') {
-    steps {
-        script {
-            withCredentials([file(credentialsId: 'k3d-config', variable: 'KUBECONFIG')]) {
-                sh script: '''
-                    export KUBECONFIG=${KUBECONFIG}
-                    for i in {1..30}; do
-                        if kubectl get pods -n dev | grep Running; then
-                            echo "Application is running!"
-                            break
-                        elif [ "$i" -eq "30" ]; then
-                            echo "Application failed to start within the expected time."
-                            currentBuild.result = 'FAILURE'
-                            exit 1
-                        else
-                            echo "Waiting for application to start..."
-                            sleep 1
-                        fi
-                    done
+  script {
+      withCredentials([file(credentialsId: 'k3d-config', variable: 'KUBECONFIG')]) {
+          sh script: '''
+              export KUBECONFIG=${KUBECONFIG}
+              for i in {1..30}; do
+                  if kubectl get pods -n dev | grep Running; then
+                      echo "Application is running!"
+                      break
+                  elif [ "$i" -eq "30" ]; then
+                      echo "Application failed to start within the expected time."
+                      currentBuild.result = 'FAILURE'
+                      exit 1
+                  else
+                      echo "Waiting for application to start..."
+                      sleep 1
+                  fi
+              done
 
-                    service_name=$(kubectl get service -n dev -o jsonpath='{.items[*].metadata.name}')
-                    kubectl run -n dev curl --image=curlimages/curl -i --rm --restart=Never -- curl ${service_name}:9898
-                ''',
-                shell: 'bash'
-            }
-        }
-    }
+              service_name=$(kubectl get service -n dev -o jsonpath='{.items[*].metadata.name}')
+              kubectl run -n dev curl --image=curlimages/curl -i --rm --restart=Never -- curl ${service_name}:9898
+          ''',
+          shell: 'bash'
+      }
+  }
 }
