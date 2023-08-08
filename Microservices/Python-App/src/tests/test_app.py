c2dfc36cbbba03e739747d6cead8f2a432001751
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-08-08 13:58:30 +0530

    Feature-X Tue  8 Aug 2023 13:58:30 IST

--- Microservices/Python-App/src/tests/test_app.py
+++ Microservices/Python-App/src/tests/test_app.py
@@ -1,13 +1,12 @@
 import unittest
 import os
-from app import app, create_server, run_server
-from unittest.mock import patch, Mock
+from app import create_server, run_server
+from unittest.mock import patch
 from http.server import HTTPServer
 from requests import get
 import threading
 
 class TestMyHandler(unittest.TestCase):
-
     def setUp(self):
         self.server_address = ('', 8888)
         self.httpd = create_server()
@@ -30,16 +29,12 @@ class TestMyHandler(unittest.TestCase):
         self.assertIn('Image_version: 1.0', response.text)
         self.assertIn('Custom_Message: test message', response.text)
 
-class TestAppFunctions(unittest.TestCase):
-
-    @patch('app.run_server')  # Mock the blocking call
-    def test_run_function(self, mock_run_server):
-        app.run()  # This should now only run create_server() and print "Setting up server..."
-        mock_run_server.assert_called_once()  # Ensure run_server() was called
-
-    def test_create_server(self):
-        httpd = app.create_server()
-        self.assertIsInstance(httpd, HTTPServer)  # Basic check to ensure the server is set up correctly
+    @patch('app.run_server')
+    def test_run(self, mock_run_server):
+        # This test will call the `run` function, but thanks to our mock, it won't actually start the server.
+        from app import run
+        run()
+        mock_run_server.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
