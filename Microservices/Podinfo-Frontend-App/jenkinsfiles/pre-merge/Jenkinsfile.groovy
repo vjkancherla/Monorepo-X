25746977ad0e60b1c90fc5efdbaf40245e781d0f
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-08-12 13:59:59 +0530

    Feature-X Sat 12 Aug 2023 13:59:59 IST

--- Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
+++ Microservices/Podinfo-Frontend-App/jenkinsfiles/pre-merge/Jenkinsfile.groovy
@@ -43,10 +43,10 @@ stage("Package-Image") {
 stage("Push-Image-To-DockerHub") {
   script {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-        sh '''
+        sh """
             echo ${DOCKER_PASSWORD} | sudo docker login -u ${DOCKER_USERNAME} --password-stdin
             sudo docker push ${getFullImageTag()}
-        '''
+        """
     }
   }
 }
@@ -73,25 +73,6 @@ stage('Test App in K3D Dev') {
       withCredentials([file(credentialsId: 'k3d-config', variable: 'KUBECONFIG')]) {
           sh script: '''
               export KUBECONFIG=${KUBECONFIG}
-
-              i=1
-              while [ "$i" -le 30 ]; do
-                  if kubectl get pods -n dev | grep -q Running; then
-                      echo "Application is running!"
-                      break
-                  elif [ "$i" -eq 30 ]; then
-                      echo "Application failed to start within the expected time."
-                      currentBuild.result = 'FAILURE'
-                      exit 1
-                  else
-                      echo "Waiting for application to start..."
-                      sleep 1
-                      i=$((i+1))
-                  fi
-              done
-
-              service_name=$(kubectl get service -n dev -o jsonpath='{.items[*].metadata.name}')
-              kubectl run -n dev curl --image=curlimages/curl -i --rm --restart=Never -- curl ${service_name}:9898
 
               i=1
               while [ "$i" -le 30 ]; do
