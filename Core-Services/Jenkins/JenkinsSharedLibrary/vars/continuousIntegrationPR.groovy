107a755584fc807e1982ec704d26ebc3741aea63
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-09-22 01:35:20 +0530

    Update: Fri Sep 22 01:35:20 IST 2023

--- Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
+++ Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
@@ -1,56 +1,58 @@
-def call(){
+def call() {
     pipeline {
         agent any
-            stages {
-                stage('Lint Code') {
-                    steps {
-                        dir('Microservices/Podinfo-Frontend-App') {
-                            echo "Lint Go Code"
-                        }
-
-                        dir('Microservices/Python-App') {
-                            echo "Lint Python Code"
-                        }
+        
+        stages {
+            
+            stage('Lint Code') {
+                steps {
+                    dir('Microservices/Podinfo-Frontend-App') {
+                        echo "Lint Go Code"
                     }
-                }
 
-                stage('Compile and Build Code') {
-                    steps {
-                        dir('Microservices/Podinfo-Frontend-App') {
-                            echo "Compile and Build Go Code"
-                        }
-
-                        dir('Microservices/Python-App') {
-                            echo "Compile and Build Python Code"
-                        }
-                    }
-                }
-
-                stage('Unit Tests') {
-                    steps {
-                        dir('Microservices/Podinfo-Frontend-App') {
-                            echo "Run Unit Tests for Go"
-                        }
-
-                        dir('Microservices/Python-App') {
-                            echo "Run Unit Tests for Python"
-                        }
-                    }
-                }
-
-                stage('Static Code Analysis') {
-                    steps {
-                        dir('Microservices/Podinfo-Frontend-App') {
-                            echo "Run Static Code Analysis"
-                        }
-
-                        dir('Microservices/Python-App') {
-                            echo "Run Static Code Analysis"
-                        }
+                    dir('Microservices/Python-App') {
+                        echo "Lint Python Code"
                     }
                 }
             }
-        }
-    }
 
+            stage('Compile and Build Code') {
+                steps {
+                    dir('Microservices/Podinfo-Frontend-App') {
+                        echo "Compile and Build Go Code"
+                    }
+
+                    dir('Microservices/Python-App') {
+                        echo "Compile and Build Python Code"
+                    }
+                }
+            }
+
+            stage('Unit Tests') {
+                steps {
+                    dir('Microservices/Podinfo-Frontend-App') {
+                        echo "Run Unit Tests for Go"
+                    }
+
+                    dir('Microservices/Python-App') {
+                        echo "Run Unit Tests for Python"
+                    }
+                }
+            }
+
+            stage('Static Code Analysis') {
+                steps {
+                    dir('Microservices/Podinfo-Frontend-App') {
+                        echo "Run Static Code Analysis"
+                    }
+
+                    dir('Microservices/Python-App') {
+                        echo "Run Static Code Analysis"
+                    }
+                }
+            }
+            
+        }
+    
+    }
 }
