43e83f26edb1f69d2e916eedd3571e3b2d83556d
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-09-23 01:14:38 +0530

    Update: Sat Sep 23 01:14:38 IST 2023

--- Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
+++ Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
@@ -4,46 +4,43 @@ def call() {
         
         stages {
             
-            // stage('Lint Code') {
-            //     steps {
-            //         sh(libraryResource('lint.sh'))
-            //     }
-            // }
+            stage('Lint Code') {
+                steps {
+                    sh(libraryResource('lint.sh'))
+                }
+            }
 
-            // stage('Compile and Build Code') {
-            //     steps {
-            //         sh(libraryResource('buildAndCompile.sh'))
-            //     }
-            // }
+            stage('Compile and Build Code') {
+                steps {
+                    sh(libraryResource('buildAndCompile.sh'))
+                }
+            }
 
-            // stage('Unit Tests') {
-            //     steps {
-            //         sh(libraryResource('runUnitTests.sh'))
-            //     }
-            // }
+            stage('Unit Tests') {
+                steps {
+                    sh(libraryResource('runUnitTests.sh'))
+                }
+            }
 
             stage('Static Code Analysis') {
                 steps {
                     script {
-                        // def sonarEnvVars
-
                         withSonarQubeEnv(installationName: 'SonarQube-on-Docker') {
                             // sonarEnvVars = env
                             sh(libraryResource('sonarScanner.sh'))
                         }
 
-                        // Export the captured environment variables for use in the shell script
-                        // env.PATH += ":${sonarEnvVars.PATH}"
-                        // env.SONAR_USER_HOME = sonarEnvVars.SONAR_USER_HOME
-                        // env.SONAR_SCANNER_OPTS = sonarEnvVars.SONAR_SCANNER_OPTS
-                        // env.SONARQUBE_SCANNER_PARAMS = sonarEnvVars.SONARQUBE_SCANNER_PARAMS
-
-                        // sh(libraryResource('sonarScanner.sh'))
+                        timeout(time: 2, unit: 'MINUTES') {
+                            def pythonQg = waitForQualityGate()
+                            if (pythonQg.status != 'OK') {
+                                error "Pipeline aborted due to quality gate failure: ${pythonQg.status}"
+                            }
+                        }
 
                         timeout(time: 2, unit: 'MINUTES') {
-                            def qg = waitForQualityGate()
-                            if (qg.status != 'OK') {
-                                error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                            def goQg = waitForQualityGate()
+                            if (goQg.status != 'OK') {
+                                error "Pipeline aborted due to quality gate failure: ${goQg.status}"
                             }
                         }
                     }
