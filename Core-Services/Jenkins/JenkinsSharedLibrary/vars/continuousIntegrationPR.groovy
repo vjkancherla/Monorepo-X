80fae87050d3a24b88bead33040374b8c11e8ec6
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-09-23 23:35:06 +0530

    Update: Sat Sep 23 23:35:06 IST 2023

--- Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
+++ Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
@@ -1,6 +1,12 @@
 def call() {
     pipeline {
         agent any
+
+         environment {
+            GIT_COMMIT_HASH = sh (script: "git log -n 1 --pretty=format:'%H'", returnStdout: true)
+            PYTHON_IMAGE_TAG = "${REGISTRY_USER}/python_app_jenkins:${GIT_COMMIT_HASH}"
+            GO_IMAGE_TAG = "${REGISTRY_USER}/go_app_jenkins:${GIT_COMMIT_HASH}"
+        }
         
         stages {
             
@@ -22,22 +28,28 @@ def call() {
                 }
             }
 
-            stage('Static Code Analysis') {
-                steps {
-                    script {
-                        withSonarQubeEnv(installationName: 'SonarQube-on-Docker') {
-                            // sonarEnvVars = env
-                            sh(libraryResource('sonarScanner.sh'))
-                        }
+            // stage('Static Code Analysis') {
+            //     steps {
+            //         script {
+            //             withSonarQubeEnv(installationName: 'SonarQube-on-Docker') {
+            //                 // sonarEnvVars = env
+            //                 sh(libraryResource('sonarScanner.sh'))
+            //             }
 
-                        timeout(time: 2, unit: 'MINUTES') {
-                            def qG = waitForQualityGate()
-                            if (qG.status != 'OK') {
-                                error "Pipeline aborted due to quality gate failure: ${qG.status}"
-                            }
-                        }
-                    }
-                }                  
+            //             timeout(time: 2, unit: 'MINUTES') {
+            //                 def qG = waitForQualityGate()
+            //                 if (qG.status != 'OK') {
+            //                     error "Pipeline aborted due to quality gate failure: ${qG.status}"
+            //                 }
+            //             }
+            //         }
+            //     }                  
+            // }
+
+            stage('Build Container') {
+                steps {
+                    sh(libraryResource('buildContainerImage ${python_image_tag} ${go_image_tag}'))
+                }
             }
             
         }
