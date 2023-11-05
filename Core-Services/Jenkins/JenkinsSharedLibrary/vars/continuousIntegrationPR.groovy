0f1d4dc0513f931e83cc978b07c0f4e9da7ab26b
Vijay Kancherla <vj_kancherla@yahoo.com>
2023-11-06 00:55:00 +0530

    Update: Mon Nov  6 00:54:59 IST 2023

--- Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
+++ Core-Services/Jenkins/JenkinsSharedLibrary/vars/continuousIntegrationPR.groovy
@@ -101,49 +101,49 @@ def call() {
                 }
             }
 
-            // stage('Build Container') {
-            //     steps {
-            //         script {
-            //             def GIT_COMMIT_HASH = sh (script: "git log -n 1 --pretty=format:'%H'", returnStdout: true)
-            //             PYTHON_IMAGE_TAG = "${PYTHON_IMAGE_REPO}:${GIT_COMMIT_HASH}"
-            //             GO_IMAGE_TAG = "${GO_IMAGE_REPO}:${GIT_COMMIT_HASH}"
+            stage('Build Container') {
+                steps {
+                    script {
+                        def GIT_COMMIT_HASH = sh (script: "git log -n 1 --pretty=format:'%H'", returnStdout: true)
+                        PYTHON_IMAGE_TAG = "${PYTHON_IMAGE_REPO}:${GIT_COMMIT_HASH}"
+                        GO_IMAGE_TAG = "${GO_IMAGE_REPO}:${GIT_COMMIT_HASH}"
 
-            //             println("${PYTHON_IMAGE_TAG} :: ${GO_IMAGE_TAG}")
+                        println("${PYTHON_IMAGE_TAG} :: ${GO_IMAGE_TAG}")
                         
-            //             withEnv(["PY_IMAGE_TAG=${PYTHON_IMAGE_TAG}", "GOO_IMAGE_TAG=${GO_IMAGE_TAG}"]) {
-            //                 sh(libraryResource('buildContainerImage.sh'))
-            //             }
-            //         }
-            //     }
-            // }
+                        withEnv(["PY_IMAGE_TAG=${PYTHON_IMAGE_TAG}", "GOO_IMAGE_TAG=${GO_IMAGE_TAG}"]) {
+                            sh(libraryResource('buildContainerImage.sh'))
+                        }
+                    }
+                }
+            }
 
-            // stage('Scan Container Image') {
-            //     steps {
-            //         echo "Scanning the built container images locally."
-            //     }
-            // }
+            stage('Scan Container Image') {
+                steps {
+                    echo "Scanning the built container images locally."
+                }
+            }
 
-            // stage('Push Container to Docker Hub') {
-            //     steps {
-            //         println("${PYTHON_IMAGE_TAG} :: ${GO_IMAGE_TAG}")
+            stage('Push Container to Docker Hub') {
+                steps {
+                    println("${PYTHON_IMAGE_TAG} :: ${GO_IMAGE_TAG}")
 
-            //         withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-            //             withEnv(["PY_IMAGE_TAG=${PYTHON_IMAGE_TAG}", "GOO_IMAGE_TAG=${GO_IMAGE_TAG}"]) {
-            //                 sh(libraryResource('publishContainerImage.sh'))
-            //             }
-            //         }
-            //     }
-            // }
+                    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        withEnv(["PY_IMAGE_TAG=${PYTHON_IMAGE_TAG}", "GOO_IMAGE_TAG=${GO_IMAGE_TAG}"]) {
+                            sh(libraryResource('publishContainerImage.sh'))
+                        }
+                    }
+                }
+            }
 
-            // stage("Helm chart dry run") {
-            //     steps {
-            //         withCredentials([file(credentialsId: 'k3d-config', variable: 'KUBECONFIG')]) {
-            //             withEnv(["PY_IMAGE_TAG=${PYTHON_IMAGE_TAG}", "GOO_IMAGE_TAG=${GO_IMAGE_TAG}"]) {
-            //                 sh(libraryResource('helmDryRun.sh'))
-            //             }
-            //         }
-            //     }
-            // }
+            stage("Helm chart dry run") {
+                steps {
+                    withCredentials([file(credentialsId: 'k3d-config', variable: 'KUBECONFIG')]) {
+                        withEnv(["PY_IMAGE_TAG=${PYTHON_IMAGE_TAG}", "GOO_IMAGE_TAG=${GO_IMAGE_TAG}"]) {
+                            sh(libraryResource('helmDryRun.sh'))
+                        }
+                    }
+                }
+            }
         }
     
     }
